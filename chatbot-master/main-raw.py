241baab547d0d6f0cfaa40c772811ab7b1841a29
Shubham Rajendra Burad <37264064+shubhamburad@users.noreply.github.com>
2020-10-01 09:54:39 +0530

    Update main-raw.py

--- chatbot-master/main-raw.py
+++ chatbot-master/main-raw.py
@@ -3,6 +3,7 @@ import os
 
 kernel = aiml.Kernel()
 
+# path for  bot_brain.brn file.
 if os.path.isfile("bot_brain.brn"):
     kernel.bootstrap(brainFile = "bot_brain.brn")
 else:
@@ -11,7 +12,7 @@ else:
 
 # kernel now ready for use
 while True:
-    message = raw_input("Tell your message to the bot: ")
+    message = raw_input("Tell your message to the bot: ") #if condition is true it will display the message else it exit from the code. 
     if message == "quit":
         exit()
     elif message == "save":
